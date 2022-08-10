50b1f734ed48b413b7c4913bd557b7e18912b934
peterent <pent@laddernow.com>
2022-08-10 10:49:11 -0400

    Added platforms.

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "MyLibrary",
-    platforms: [ .iOS(.v15) ],
+    platforms: [ .iOS(.v15), .macOS(.v12) ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -21,7 +21,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "MyLibrary",
-            dependencies: []),
+            dependencies: [],
+            path: "Sources"),
         .testTarget(
             name: "MyLibraryTests",
             dependencies: ["MyLibrary"]),
