9229eb773a034e24f9007acc7c2e8a6942ba9677
peterent <pent@laddernow.com>
2022-08-10 10:28:13 -0400

    Added WelcomeView.

---
+++ Sources/MyLibrary/WelcomeView.swift
@@ -0,0 +1,23 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by PETER ENT on 8/10/22.
+//
+
+import SwiftUI
+
+public struct WelcomeView: View {
+    public var body: some View {
+        Text("Welcome to ") +
+        Text("My Library")
+            .foregroundColor(.green)
+            .font(.headline)
+    }
+}
+
+struct WelcomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        WelcomeView()
+    }
+}
